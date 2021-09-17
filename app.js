b9cb4186b513d88bd91cf7aa2f366ef70773527b
junhao-tang <junhao.tang@outlook.com>
2021-09-17 23:35:05 +0800

    expanded game field. further testing

--- app.js
+++ app.js
@@ -1,4 +1,85 @@
 const main = () => {
+  // Methods
+  const startGame = () => {
+    //(playerName, gameMode)
+    moleTrigger();
+  };
+
+  const endGame = () => {};
+
+  const randomMinMax = (min, max) => {
+    // Generates a random value between defined min, max
+    return Math.ceil(Math.random() * (max - min)) + min;
+  };
+
+  const moleTrigger = () => {
+    console.log("moleTrigger called");
+    //set random duration between 0.5-2.0s
+    let duration = randomMinMax(500, 2000);
+    //set delay in 0.1s divisions between 0.3-2.0s
+    let delay = randomMinMax(300, 2000);
+    setTimeout(moleUp(duration), delay);
+  };
+
+  const moleUp = (duration) => () => {
+    console.log("moleup called");
+    const tileId = Math.floor(Math.random() * 9); //gridsize variable
+    // $tiles.eq(tileId).addClass("up");
+    renderGameBoard(tileId, "up");
+    setTimeout(moleDown(tileId), duration);
+    console.log("-------------");
+  };
+
+  const moleDown = (tileId) => () => {
+    console.log("moleDown called");
+    // $tiles.eq(tileId).removeClass("up");
+    renderGameBoard(tileId, "down");
+    moleTrigger();
+    console.log("-------------");
+  };
+
+  const hammerPress = (e) => {
+    console.log("hammerPress called");
+    if ($(e.currentTarget).hasClass("up")) {
+      // $(e.currentTarget).addClass("hit").removeClass("up");
+      const tileId = $(e.currentTarget).attr("id");
+      renderGameBoard(tileId, "hit");
+      setTimeout(() => {
+        $(e.currentTarget).removeClass("hit");
+      }, 200);
+      score++;
+      renderScore(score);
+    } else if (!$(e.currentTarget).hasClass("hit")) {
+      missed++;
+      console.log(randomMinMax(500, 2000));
+    }
+    console.log(`Score: ${score} \r Missed: ${missed}`); //* __remove__
+  };
+
+  // Rendering methods
+  const renderStartScreen = () => {};
+  const renderGameScreen = (gameMode, player) => {};
+  const renderScore = (score) => {
+    $score.text(score);
+  };
+  const renderGameBoard = (id, state) => {
+    if (state === "up") {
+      $(`#${id}`).addClass("up");
+      console.log("renderGameboard up");
+    } else if (state === "down") {
+      $(`#${id}`).removeClass("up");
+      console.log("renderGameboard down");
+    } else if (state === "hit") {
+      $(`#${id}`).addClass("hit").removeClass("up");
+      console.log("renderGameboard hit");
+    } else if (state === "hit") {
+      $(`#${id}`).removeClass("unhit");
+      console.log("renderGameboard unhit");
+    }
+  };
+  const renderTimer = () => {};
+  const renderGameOver = () => {};
+
   // Init data
   const grid = {
     id: "",
@@ -17,72 +98,12 @@ const main = () => {
   const $normal = $("#btn-normal");
   const $hard = $("#btn-hard");
   const $play = $("#btn-play");
-  let $tiles = $(".tile");
-
-  // Methods
-  const startGame = () => {
-    //(playerName, gameMode)
-    moleTrigger();
-  };
-
-  const endGame = () => {};
-
-  const moleTrigger = () => {
-    console.log("moleTrigger called");
-    //set random duration between 0.5-2.0s
-    let duration = Math.ceil(Math.random() * 15) * 100 + 500;
-    //set delay in 0.1s divisions between 0.3-3.0s
-    let delay = Math.ceil(Math.random() * 30) * 100;
-    setTimeout(moleUp(duration), delay);
-  };
-
-  const moleUp = (duration) => () => {
-    console.log("moleup called");
-    const tileId = Math.floor(Math.random() * 3); //gridsize variable
-    console.log(tileId);
-    $tiles.eq(tileId).addClass("up");
-    renderGameScreen();
-    setTimeout(moleDown(tileId), duration);
-    console.log("-------------");
-  };
-
-  const moleDown = (tileId) => () => {
-    console.log("moleDown called");
-    $tiles.eq(tileId).removeClass("up");
-    moleTrigger();
-    console.log("-------------");
-  };
-
-  const hammerPress = () => {
-    console.log("hammerPress called");
-    if ($(e.currentTarget).hasClass("up")) {
-      $(e.currentTarget).addClass("hit");
-      setTimeout(() => {
-        $(e.currentTarget).removeClass("hit");
-      }, 100);
-      score++;
-      renderScore();
-    } else if (!$(e.currentTarget).hasClass("hit")) {
-      missed++;
-    }
-  };
-
-  // Rendering methods
-  const renderStartScreen = () => {};
-  const renderGameScreen = (gameMode, player) => {};
-  const renderGameBoard = (id, state) => {
-    if (state === "up") {
-      $tiles.eq[id].addClass("up");
-    } else if (state === "down") {
-      $tiles.eq[id].removeClass("up");
-    } else if (state === "hit") {
-      $tiles.eq[id].addClass("hit");
-    }
-  };
-  const renderTimer = () => {};
-  const renderGameOver = () => {};
+  const $tiles = $(".tile");
+  const $score = $("#score");
 
   startGame();
+
+  $tiles.on("mousedown", hammerPress);
 };
 
 $(main);
